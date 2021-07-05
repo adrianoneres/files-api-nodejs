7c8325a08e5658ad8e32e9b4f3c6943efa471437
Adriano Neres <ah.driano@gmail.com>
2021-07-04 22:21:14 -0400

    build: configuring the basic structure

---
+++ README.md
@@ -0,0 +1,49 @@
+# Files API
+
+## 1. Development environment
+
+- [NVM](https://github.com/nvm-sh/nvm)
+- [Yarn](https://yarnpkg.com)
+
+## 2. Setup
+
+### 2.1. Add your local configuration
+
+Set up your local environment configurations in the file `.env`.
+If you don't have a `.env` file in the root directory, please copy the `.env.example`, rename it to `.env` and fill the properties values with your environment details.
+
+### 2.2. Make sure you have the proper `Node.js` version installed:
+
+```
+
+nvm use && nvm install
+
+```
+
+### 2.3. Install dependencies
+
+```
+yarn
+```
+
+## 3. Development
+
+To start the development server:
+
+```
+yarn dev
+```
+
+To run tests use:
+
+```
+yarn test
+```
+
+## 4. Build
+
+To build project to `dist` directory:
+
+```
+yarn build
+```
---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,37 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import '@shared/containers';
+import cors from 'cors';
+import { errors } from 'celebrate';
+
+import routes from '@shared/infra/http/routes';
+import { AppError } from '@shared/errors/AppError';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+app.use(errors());
+
+const host = process.env.APP_HOST || 'localhost';
+const port = process.env.APP_PORT ? Number(process.env.APP_PORT) : 3333;
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+  return response.status(500).json({
+    status: 'error',
+    message: err.message,
+  });
+});
+
+app.listen(port, host, () => {
+  console.log(`Server started on http://${host}:${port}!`);
+});
